80a326afb09a825e5b3a19d9cb342aa2f208dfdc
JimyHsu <airball91327@hotmail.com>
2019-09-27 16:23:00 +0800

    1. Create Keep Models

--- EDIS/EDIS/Models/KeepModels/KeepDtlModel.cs
+++ EDIS/EDIS/Models/KeepModels/KeepDtlModel.cs
@@ -27,8 +27,8 @@ namespace EDIS.Models.KeepModels
         [Display(Name = "保養費用")]
         [DisplayFormat(DataFormatString = "{0:N0}")]
         public decimal? Cost { get; set; }
-        [Display(Name = "[是][否]為統包")]
-        public string NotInExceptDevice { get; set; }
+        //[Display(Name = "[是][否]為統包")]
+        //public string NotInExceptDevice { get; set; }
         [Display(Name = "[是][否]為器械")]
         public string IsInstrument { get; set; }
         [DataType(DataType.Date)]
--- EDIS/EDIS/Models/KeepModels/KeepListViewModel.cs
+++ EDIS/EDIS/Models/KeepModels/KeepListViewModel.cs
@@ -11,7 +11,7 @@ namespace EDIS.Models.KeepModels
         public string DocId { get; set; }
         [Display(Name = "財產編號")]
         public string AssetNo { get; set; }
-        [Display(Name = "儀器名稱")]
+        [Display(Name = "物品名稱")]
         public string AssetName { get; set; }
         [Display(Name = "廠牌")]
         public string Brand { get; set; }
