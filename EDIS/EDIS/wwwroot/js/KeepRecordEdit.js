2cd8ef8d791339520f4f1afd10af334b94c135c2
JimyHsu <airball91327@hotmail.com>
2019-10-09 14:12:26 +0800

    1. Add asp-append-vesion for all js related views.

---
+++ EDIS/EDIS/wwwroot/js/KeepRecordEdit.js
@@ -0,0 +1,15 @@
+﻿function smgKEEPRECORD(data) {
+    if (data.success === false) {
+        alert(data.error);
+        $.Toast.hideToast();
+    }
+    else {
+        alert("儲存成功!");
+        $.Toast.hideToast();
+    }
+}
+
+$(function () {
+
+
+});
